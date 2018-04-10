cfa63301a8271286a32dee746bd2bc19fb2eb0b5
Rezazadeh Khorakpaz <mohammadali.rezazadehkhorakpaz@monster.com>
2018-04-10 15:37:56 +0800

    finish POC

--- 3DModel/3DModel/MainWindow.xaml.cs
+++ 3DModel/3DModel/MainWindow.xaml.cs
@@ -2,11 +2,13 @@
 using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -24,12 +26,27 @@ namespace _3DModel
     public partial class MainWindow : Window
     {
         ModelVisual3D device3D;
+        Popup loadPopup = new Popup();
+
+        public ObservableCollection<Sticker> UserStickers { get; set; }
+
+        public Sticker UserSticker { get; set; }
+
+        public Point3D? StickerPoint { get; set; }
 
         public MainWindow()
         {
             InitializeComponent();
+            Loaded += MainWindow_Loaded;
+            UserStickers = new ObservableCollection<Sticker>();
+
         }
-        
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            viewer.ZoomExtents();
+        }
+
         private Model3D DisplayDesign(string modelPath)
         {
             Model3D device = null;
@@ -73,20 +90,12 @@ namespace _3DModel
 
         private void BtnReset_Click(object sender, RoutedEventArgs e)
         {
-            if(device3D != null && viewer.Children.Contains(device3D))
-            {
-                bool tmp = viewer.Children.Remove(device3D);
-            }
-        }
+            //if(device3D != null && viewer.Children.Contains(device3D))
+            //{
+            //    bool tmp = viewer.Children.Remove(device3D);
+            //}
 
-        private void viewer_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            var point = viewer.FindNearestPoint(e.GetPosition(viewer));
-
-            if (point.HasValue)
-            {
-                viewer.Children.Add(new SphereVisual3D() { Center = (Point3D)point, Radius = 5, Material = Materials.Green });
-            }
+            viewer.Children.Clear();
         }
 
         private void viewer_MouseMove(object sender, MouseEventArgs e)
@@ -95,9 +104,116 @@ namespace _3DModel
 
             if(point.HasValue)
             {
-                txtCoordinate.Text = String.Format(@"X: {0} | Y: {1} | Z: {2} ", 
-                    point.Value.X, point.Value.Y, point.Value.Z);
+                txtX.Text = point.Value.X.ToString();
+                txtY.Text = point.Value.Y.ToString();
+                txtZ.Text = point.Value.Z.ToString();
+
+                if(CheckCurrentPoint((Point3D)point))
+                {
+                    lblFlag.Content = "In bound show !!!";
+                    ShowDetail();
+                }
+                else
+                {
+                    lblFlag.Content = "Out bound show !!!";
+                }
+
             }
+        }
+
+        private void viewer_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.StickerPoint = viewer.FindNearestPoint(e.GetPosition(viewer));
+
+                if (this.StickerPoint.HasValue)
+                {
+                    myPopup.IsOpen = true;
+
+
+                }
+
+            }
+        }
+
+        //private void Button_Click(object sender, RoutedEventArgs e)
+        //{
+        //    myPopup.IsOpen = true;
+        //}
+
+        private void BtnClose_Click (object sender, RoutedEventArgs e)
+        {
+            var glue = new SphereVisual3D() { Center = (Point3D)this.StickerPoint, Radius = 0.2, Material = Materials.Yellow };
+            
+            viewer.Children.Add(glue);
+
+            UserStickers.Add(new Sticker()
+            {
+                ID = Guid.NewGuid(),
+                SelectedPoint = (Point3D)this.StickerPoint,
+                AttachedSticker = glue,
+                UserMessage = txtUserComment.Text
+            });
+
+            lstSelectedPoints.ItemsSource = UserStickers;
+
+            myPopup.IsOpen = false;
+        }
+
+        private bool CheckCurrentPoint(Point3D point)
+        {
+            foreach(var item in UserStickers)
+            {
+                if(
+                    (point.X <= (item.SelectedPoint.X + 0.2) && point.X >= (item.SelectedPoint.X - 0.2))
+                    && (point.Y <= (item.SelectedPoint.Y + 0.2) && point.Y >= (item.SelectedPoint.Y - 0.2))
+                    && (point.Z <= (item.SelectedPoint.Z + 0.2) && point.Z >= (item.SelectedPoint.Z - 0.2))
+                )
+                {
+                    this.UserSticker = item;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private void ShowDetail()
+        {
+            if(this.UserSticker != null)
+            {
+                loadPopup.PopupAnimation = PopupAnimation.Fade;
+
+                loadPopup.Width = 200;
+                loadPopup.Height = 200;
+                loadPopup.Placement = PlacementMode.MousePoint;
+
+                Grid grid = new Grid();
+
+                TextBlock popupText = new TextBlock();
+                popupText.Text = this.UserSticker.UserMessage;
+                popupText.Background = Brushes.LightBlue;
+                popupText.Foreground = Brushes.Black;
+
+                Button btnLoadPopUpClose = new Button();
+                btnLoadPopUpClose.Width = 75;
+                btnLoadPopUpClose.Height = 23;
+                btnLoadPopUpClose.Content = "Close";
+                btnLoadPopUpClose.Click += btnLoadPopUpClose_Click1;
+
+                grid.Children.Add(popupText);
+                grid.Children.Add(btnLoadPopUpClose);
+
+                loadPopup.Child = grid;
+
+                loadPopup.IsOpen = true;
+            }
+        }
+
+        private void btnLoadPopUpClose_Click1(object sender, RoutedEventArgs e)
+        {
+            loadPopup.IsOpen = false;
         }
     }
 }
---
+++ 3DModel/3DModel/Sticker.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Media3D;
+
+namespace _3DModel
+{
+    public class Sticker
+    {
+        public Guid ID { get; set; }
+
+        public Point3D SelectedPoint { get; set; }
+
+        public Visual3D AttachedSticker { get; set; }
+
+        public string X
+        {
+            get { return this.SelectedPoint.X.ToString(); }
+        }
+
+        public string Y {
+            get { return this.SelectedPoint.Y.ToString(); }
+        }
+
+        public string Z { 
+            get { return this.SelectedPoint.Z.ToString(); }
+        }
+
+        public string UserMessage{ get; set; }
+    }
+}
